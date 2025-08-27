bfab49ee68f938bea7774f0b4edb9a6f521402e4
mohamed hatem <mohm77778727@gmail.com>
2025-08-27 23:28:20 +0300

    first commit

---
+++ lib/views/home.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:pro2/views/splash.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Splash(),);
+  }
+}
