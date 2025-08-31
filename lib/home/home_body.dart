d6d0730e2f8c1cbdf5cef271cab35cfa18024674
mohamed hatem <mohm77778727@gmail.com>
2025-08-31 21:37:08 +0300

    add category model and add  CarouselSlider category

--- lib/home/home_body.dart
+++ lib/home/home_body.dart
@@ -1,5 +1,6 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:pro2/models/category_model.dart';
 
 class HomeBody extends StatefulWidget {
   const HomeBody({super.key});
@@ -13,6 +14,13 @@ class _HomeBodyState extends State<HomeBody> {
     'assets/image/Slider1.png',
     'assets/image/Slider2.png',
     'assets/image/Slider3.png',
+  ];
+
+  List<CategoryModel> categoryModel = [
+    CategoryModel(name: 'Fruits', image: 'assets/image/category4.png'),
+    CategoryModel(name: 'Milk & egg', image: 'assets/image/category3.png'),
+    CategoryModel(name: 'Beverages', image: 'assets/image/category2.png'),
+    CategoryModel(name: 'Laundry', image: 'assets/image/category1.png'),
   ];
 
   @override
@@ -31,6 +39,39 @@ class _HomeBodyState extends State<HomeBody> {
             autoPlayAnimationDuration: Duration(milliseconds: 800),
             enlargeCenterPage: true,
             autoPlayCurve: Curves.fastOutSlowIn,
+          ),
+        ),
+
+        SizedBox(height: 12),
+
+        CarouselSlider.builder(
+          itemCount: categoryModel.length,
+          itemBuilder:
+              (BuildContext context, int itemIndex, int pageViewIndex) {
+                return Column(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(13),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(40),
+                        color: Color(0xffF6F6F6),
+                      ),
+                      child: Image.asset(
+                        categoryModel[itemIndex].image,
+                        width: 50,
+                        height: 50,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Text(categoryModel[itemIndex].name),
+                  ],
+                );
+              },
+          options: CarouselOptions(
+            height: 200,
+            viewportFraction: 0.3,
+            enableInfiniteScroll: false,
+            padEnds: false,
           ),
         ),
       ],
--- lib/home/home_view.dart
+++ lib/home/home_view.dart
@@ -9,6 +9,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xffFFFFFF),
       appBar: AppBar(
         leading: SizedBox.shrink(),
         leadingWidth: 0,
